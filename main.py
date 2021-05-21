6a84d11e733231deac0dff510409d1ba8fccb5fb
Raynhard Anthony Yray <raynhardanthonyyray2001@gmail.com>
2021-05-21 13:43:34 +0800

    revised main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import random
-result = {
-    "rock": "paper",
-    "paper": "scissors",
-    "scissors": "rock"
-}
 
 choices = ( "rock", "paper", "scissors" )
 
-user_input = input("rock, paper, OR scissors: ")
+
+user_input = input("ENTER YOUR CHOICE [1]rock, [2]paper, OR [3]scissors: ")
 cpu_input = random.choices(choices)
 
 print("\nYOUR CHOICE: " + user_input)
-print("\nYOUR OPPONENT'S CHOICE: " + cpu_input[0])
+print("YOUR OPPONENT'S CHOICE: " + cpu_input[0])
 
 if (user_input == cpu_input[0]):
-    print("RESULT: It's a Tie!")
-
+    print("\nRESULT: It's a Tie!")
+elif (user_input == "rock"):
+    if (cpu_input == "paper"):
+        print("\nRESULT: CPU WON!")
+    else:
+        print("\nRESULT YOU WON!")
+elif (user_input == "paper"):
+    if (cpu_input == "scissors"):
+        print("\nRESULT: CPU WON!")
+    else:
+        print("\nRESULT YOU WON!")
+elif (user_input == "scissors"):
+    if (cpu_input == "rock"):
+        print("\nRESULT: CPU WON!")
+    else:
+        print("\nRESULT YOU WON!")
+else:
+    print("\nTRY AGAIN!")
